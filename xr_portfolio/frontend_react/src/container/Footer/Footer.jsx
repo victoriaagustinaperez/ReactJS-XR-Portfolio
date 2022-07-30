c4433b258d9ef6cd235fc6a5e7f203c998015cd1
Victoria Pérez <victoriathecompeng@gmail.com>
2022-07-30 09:22:02 -0400

    tweaked style, removed assets

--- xr_portfolio/frontend_react/src/container/Footer/Footer.jsx
+++ xr_portfolio/frontend_react/src/container/Footer/Footer.jsx
@@ -52,36 +52,11 @@ const Footer = () => {
           <a href="tel:+1 (305) 905-5305" className="p-text">+1 (305) 904-5305</a>
         </div>
       </div>
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-          </div>
-          <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-          </div>
-          <div>
-            <textarea
-              className="p-text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
-          </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-        </div>
-      ) : (
-        <div>
-          <h3 className="head-text">
-            Thank you for getting in touch!
-          </h3>
-        </div>
-      )}
-        <div className="copyright">
-        <p className="p-text">@xrbyvictoria.com since 2022.</p>
-        <p className="p-text">All rights reserved.</p>
-        </div>
+
+      <div className="copyright">
+      <p className="p-text">@xrbyvictoria.com since 2022.</p>
+      <p className="p-text">All rights reserved.</p>
+      </div>
     </>
   );
 };
