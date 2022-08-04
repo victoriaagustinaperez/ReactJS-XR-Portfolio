d6b99bfd175b2768037f29ae4e17f1805d6ac87d
Victoria Pérez <victoriathecompeng@gmail.com>
2022-08-04 00:35:51 -0400

    added Github icon

--- xr_portfolio/frontend_react/src/components/SocialMedia.jsx
+++ xr_portfolio/frontend_react/src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsLinkedin, BsInstagram, BsDiscord} from 'react-icons/bs';
+import { BsLinkedin, BsGithub, BsInstagram, BsDiscord} from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
@@ -7,6 +7,11 @@ const SocialMedia = () => (
       <a href="https://www.linkedin.com/in/victoriaagustinaperez/" target="_blank" rel="noreferrer">
         <BsLinkedin color="#0e76a8"/>
         </a>
+    </div>
+    <div>
+          <a href="https://github.com/victoriaagustinaperez" target="_blank" rel="noreferrer">
+          <BsGithub color="000"/>
+          </a>
     </div>
     <div>
       <a href="https://www.instagram.com/xrbyvictoria/?hl=en" target="_blank" rel="noreferrer">
